09d078df56f452160bd899eda6a35686408cea61
Melissa Arliss <marliss3@gmail.com>
2019-09-29 13:02:46 -0400

    hw1 solution

--- hw/hw-1.py
+++ hw/hw-1.py
@@ -14,7 +14,6 @@ BOP! (pretty sure this is a space!)
 """
 
 
-# H
 beeps1 = 2
 boops1 = 6
 letter1 = beeps1 + boops1
@@ -24,3 +23,43 @@ beeps2 = 0
 boops2 = 5
 letter2 = beeps2 + boops2
 print(letter2)
+
+beeps3 = 9
+boops3 = 3
+letter3 = beeps3 + boops3
+print(letter3)
+
+beeps4 = 4
+boops4 = 8
+letter4 = beeps4 + boops4
+print(letter4)
+
+beeps5 = 10
+boops5 = 5
+letter5 = beeps5 + boops5
+print(letter5)
+
+beeps6 = 11
+boops6 = 12
+letter6 = beeps6 + boops6
+print(letter6)
+
+beeps7 = 5
+boops7 = 5
+letter7 = beeps7 + boops7
+print(letter7)
+
+beeps8 = 1
+boops8 = 17
+letter8 = beeps8 + boops8
+print(letter8)
+
+beeps9 = 5
+boops9 = 7
+letter9 = beeps9 + boops9
+print(letter9)
+
+beeps10 = 4 
+boops10 = 0
+letter10 = beeps10 + boops10
+print(letter10)
