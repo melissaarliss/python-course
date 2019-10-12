acd76da23fb7fdc67948d963be9589dc2d634d37
Melissa Arliss <marliss3@gmail.com>
2019-10-12 12:51:29 -0400

    updated game

--- hw/hw-4/rps.py
+++ hw/hw-4/rps.py
@@ -3,27 +3,45 @@ import random
 choices = ["rock", "paper", "scissors"] 
 
 def game_time():
-	player_choice = input("Make your choice: rock, paper, or scissors.")
-	player_choice = player_choice.lower().strip()
+	player_choice = input("Make your choice: rock, paper, or scissors. ").lower().strip()
 	while player_choice not in choices:
-		player_choice = input("Try again: the choices are rock, paper, or scissors.")	
-		player_choice = player_choice.lower().strip()
+		player_choice = input("Try again: the choices are rock, paper, or scissors. ").lower().strip()
 	print(f"Your choice is {player_choice}.")
+
 	computer_choice = random.choice(choices)
 	print(f"The computer's choice is {computer_choice}.")
-	if player_choice == "rock" and computer_choice == "paper":
-		print("The computer won.")
-	elif player_choice == "rock" and computer_choice == "scissors":
-		print("You won!")	
-	elif player_choice == "scissors" and computer_choice == "paper":
-		print("You won!")
-	elif player_choice == "scissors" and computer_choice == "rock":
-		print("The computer won.")	
-	elif player_choice == "paper" and computer_choice == "rock":
-		print("You won!")
-	elif player_choice == "paper" and computer_choice == "scissors":
-		print("The computer won.")	
-	else:
-		print("It's a tie!")	
 
-game_time()		
+	player_score = 0
+	computer_score = 0
+
+	if player_choice == computer_choice:
+		print("It's a tie!")
+	elif player_choice == "rock":
+		if computer_choice == "paper":
+			print("The computer won.")
+			computer_score += 1
+		else:
+			print("You won!")
+			player_score += 1	
+	elif player_choice == "paper":
+		if computer_choice == "scissors":
+			print("The computer won.")
+			computer_score += 1	
+		else:
+			print("You won!")
+			player_score += 1	
+	elif player_choice == "scissors":
+		if computer_choice == "rock":
+			print("The computer won.")
+			computer_score += 1	
+		else:
+			print("You won!")
+			player_score += 1
+
+	print(f"You have {player_score} points and the computer has {computer_score} points.")
+	if input(("Want to play again? Enter yes or no. ")).lower().strip() == "yes":
+		game_time()
+	else:
+		print("Goodbye!")
+
+game_time()
