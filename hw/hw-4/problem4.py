779f15eb27d75b55544f3e7e7ef0bc3a615f029e
Melissa Arliss <marliss3@gmail.com>
2019-10-12 12:19:28 -0400

    changed problem4

--- hw/hw-4/problem4.py
+++ hw/hw-4/problem4.py
@@ -8,10 +8,12 @@ num_1 = int(input("What is the first number? "))
 num_2 = int(input("What is the second number? "))
 
 if operation == "add":
-	print(f"The answer is {num_1 + num_2}.")
+	answer = num_1 + num_2
 elif operation == "sub":
-	print(f"The answer is {num_1 - num_2}.")
+	answer = num_1 - num_2
 elif operation == "mult":
-	print(f"The answer is {num_1 * num_2}.")
+	answer = num_1 * num_2
 elif operation == "div":
-	print(f"The answer is {num_1 / num_2}.")		
+	answer = num_1 / num_2
+
+print(f"The answer is {answer}.")		
